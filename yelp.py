caa3ad7c82eac80e45a26c901d1dfebfcc44ef76
Weiyi Zhang <weiyizha@andrew.cmu.edu>
2020-10-22 15:44:45 +0800

    Modify the written.

--- yelp.py
+++ yelp.py
@@ -7,11 +7,11 @@ import altair as alt
 from wordcloud import WordCloud
 import covidcast
 import geopandas as gpd
-import us
-from geopy.geocoders import Nominatim
-import addfips
-from vega_datasets import data
-import time
+# import us
+# from geopy.geocoders import Nominatim
+# import addfips
+# from vega_datasets import data
+# import time
 from utils import total_covid_feature, cate_list_multi, cate_list, cate_func, find_category, get_category, get_highlight_info, get_bool_df, get_bool_df_summary
 
 @st.cache  # load data from url, when submitting
@@ -52,13 +52,14 @@ def load_data_from_local():
 def get_join_dataset(dataset1, dataset2, col):
     return pd.merge(dataset1, dataset2, on=col)
 
-
+'''
 @st.cache
 # get the state name to id dic
 def get_state_ref():
     state_ref = pd.read_json(data.income.url)[['name', 'id']].groupby(['name']).mean()  
     # city_ref = pd.read_json("dataset/yelp_academic_dataset_covid_features.json", lines=True)
     return state_ref
+'''
 
 
 
@@ -151,9 +152,9 @@ def show_covid_feature_multi_relationship(total_covid_feature, yelp_covid_bool_d
 def close_for_how_long(yelp_join):
     st.markdown("From now on, we retrive the original information in **Temporary Closed Until**, **Covid Banner**, and **highlights**.")
     
-    st.write("First, let's see when do these temporarly closed businesses plan to reopen in June 10. There are {} businesses uploading closure notification.".format(sum(yelp_covid_df['Temporary Closed Until'] != 'FALSE')))
+    st.write("First, let's see when did these temporarly closed businesses plan to reopen in June 10. There are {} businesses uploading closure notification.".format(sum(yelp_covid_df['Temporary Closed Until'] != 'FALSE')))
     
-    st.write("You may select certain category you are interested in from the bottom box, and brush certain time sub-interval to from the upper figure.")
+    st.write("You may select certain category you are interested in from the bottom box, and brush certain time sub-interval from the upper figure.")
 
     close_time = yelp_join[yelp_join['Temporary Closed Until'] != 'FALSE']['Temporary Closed Until']
     close_time = list(close_time)
@@ -185,8 +186,12 @@ def close_for_how_long(yelp_join):
     ).add_selection(picked).properties(height=300, width=500)
 
     st.write(chart)
+    st.write("It is interesting that the planned reopen time is quite concentrated, most during June and July, and on the start or end of a certain month.")
 
 def what_covid_banner_say(yelp_join, business_category_info):
+
+    st.write("Next, let's turn to Covid Banner. You may want see what words frequently appear in that banner, in whole, or in certain category.")
+
     feature = st.selectbox("Choose a category you are interested in: ", ['whole'] + cate_list_multi)
     
     if feature == 'whole': 
@@ -200,49 +205,67 @@ def what_covid_banner_say(yelp_join, business_category_info):
     if feature == 'whole':
         cate_str = "in all"
 
-    st.write("Next, let's turn to Covid Banner. There are {} businesses {} uploading their Covid Banner.".format(total_num, cate_str))
-    st.write("You may want see what words frequently appear in that banner, in whole, or in certain category.")
+    st.write("There are {} businesses {} uploading their Covid Banner.".format(total_num, cate_str))
     w = WordCloud(background_color='white', width=600, height=300)
     banner_str = ' '.join(banner)
     w.generate(banner_str)
     c = w.to_image()
     st.image(c)
 
+    st.write("In all, the most important words and **will** and **open**. Businesses are expressing they are open and they would make special reaction towards COVID-19. Also, **appointment** and **safe/safety** are frequent, showing their concern towards health gurantee. You are encouraged to explore more into different categories.")
+
 def what_are_highlights(business_highlight_info_short):
-    st.write("Now let's take a look at hightlights, which are put in the business information page. There are 7045 businesses in all updating their highlights in response to Covid 19. We can see what highlights are about.")
+    st.write("Then let's take a look at **hightlights**, which are highlighted short information posted on the business home page. There are 7045 businesses in all updating their highlights in response to Covid 19. We may see what highlights are about.")
     st.write("Below, we extract 11 COVID-19 related highlights, and show their counts decomposing in different categories.")
-    st.write("You may select one highlight and see in more detailed about its decomposition in the second figure. You may also select one particular category and compare highlight types of business belong to that category.")
+    st.write("You may select one highlight and see in more detailed about its decomposition below.")
+    st.write("You may also select one particular category from the right legend by click and compare how highlight types vary in that category.")
+
     selected = alt.selection_multi(encodings=['y'], resolve='intersect')
-    selected_cate = alt.selection_multi(encodings=['y'])
+    selected_cate = alt.selection_multi(fields=['categories'], bind='legend')
+
     chart = alt.Chart(business_highlight_info_short).mark_bar().encode(
-        alt.X('sum(count)'),
+        alt.X('sum(count)', sort='-color'),
         alt.Y('highlight:N', sort='-x'),
         alt.Tooltip(['highlight', 'sum(count)']),
-        color = alt.condition(selected_cate, 'categories', alt.value("lightgray")),
-    ).add_selection(selected)& alt.Chart(business_highlight_info_short).mark_bar().encode(
+        color = alt.condition(selected_cate, alt.Color(
+            'categories:N',
+        ), alt.value("lightgray")),
+    ).add_selection(selected_cate).add_selection(selected)& alt.Chart(business_highlight_info_short).mark_bar().encode(
         alt.Y('categories:N'),
         alt.X('sum(count)'),
-    ).add_selection(selected_cate).transform_filter(selected)
+        alt.Color('categories')
+    ).transform_filter(selected)
 
     st.write(chart)
+    st.write("Clearly, different categories are focus on different highlights. Reataurants and food, they are showing they support dilivery or takeout, and similarly shopping businesses provide shipping service. For service business, medical and home service focus on remote service, while beauty and spas can only send gift cards during COVID-19.")
 
 def show_business_in_category(yelp_covid_bool_df, business_category_info):
     
     cat_info_cnt = pd.DataFrame()
-    feature = st.selectbox("Select a Covid feature you are interested in:  ", yelp_covid_bool_df.columns[1:])
+    feature = st.selectbox("Select a Covid feature you are interested in: ", total_covid_feature)
 
     for cate in cate_list_multi:
         cat_info_cnt[cate] = yelp_covid_bool_df[business_category_info[cate]].groupby(feature).agg({'business_id':'count'})['business_id']
     
-    cat_info_cnt = cat_info_cnt.transpose()
-    st.bar_chart(pd.DataFrame(
-        np.array(cat_info_cnt),
-        columns=['False', 'True'],
-        index=cate_list_multi
-    ))
+    cat_info_cnt = cat_info_cnt.stack().reset_index(level=[0,1])
+    cat_info_cnt.columns = [feature, 'categories', 'count']
+    chart = alt.Chart(cat_info_cnt).mark_bar().encode(
+        alt.X('count'),
+        alt.Y('categories:N', sort='-x'),
+        alt.Color(feature+':N'),
+        alt.Tooltip(['count', feature]),
+        order = alt.Order(
+            feature,
+            sort='descending'
+        )
+    ).properties(height=200, width=700)
+    st.altair_chart(chart)
+
+    st.write("Of course, different types of businesses react differently. For example, you can see a huge part of Reastaurants and Food support **delivery and takeout**, while **request a quote** is nearly exclusively supported by Home Services.")
+    
 
 def show_quality_summary(yelp_join):
-    st.write("We first look at the distribution of star ratings and review counts in our dataset.")
+    st.write("We first look at the distribution of **star ratings** and **review counts** in our dataset. Recall that the quality data is collected in March 2020, when the COVID was not that a serious concern, and thus could be used as a previous quality measure.")
 
     out_start = yelp_join.groupby(['stars']).agg({'business_id':'count'})
     df_star = pd.DataFrame()
@@ -272,10 +295,9 @@ def show_quality_summary(yelp_join):
     st.markdown("You may find that **stars** are relatively scattered, while for **review counts**, they are pretty concentrated, and actually, though the most popular business can have more than 10,000 reviews, the 95 percentile of review count is 145.")
 
 
-
 def quality_vs_covid_feature(yelp_covid_bool_df, yelp_join):
 
-    st.markdown("Now, let's explore into the relationship among **stars**, **review counts** and **COVID features**. It is likely that business states are affected by their previous quality. Recall that the quality data is collected in March 2020, when the COVID was not that a serious concern.")
+    st.markdown("Now, let's explore into the relationship among **stars**, **review counts** and **COVID features**. It is likely that business states are affected by their previous quality.")
     st.write("Due to the huge size of original dataset, we use sampling strategy. Let's start by choosing a **sample size**!")
 
     sample_size = st.slider("Select how mamy points you want to sample for each case: ", min_value=50, max_value=500, value=100)
@@ -308,8 +330,10 @@ def quality_vs_covid_feature(yelp_covid_bool_df, yelp_join):
         opacity=alt.condition(selected, alt.value(1), alt.value(0.2))
     ).add_selection(selected).properties(width=600).interactive()
 
-    st.write("Now you have your graph! Try to click one point or a True/False (1/0) on the right to highlight a certain group.")
+    st.write("Now you have your graph! Try to double click one point or a True/False (1/0) on the right to highlight a certain group.")
     st.write(chart)
+
+    st.write("You may see that stars do not influence much, while more businesses are more likely to react.")
     
 
 yelp_business_df, yelp_covid_df, yelp_join = load_data_from_local() #load_data_from_url()
@@ -360,7 +384,7 @@ show_covid_feature_multi_relationship(total_covid_feature, yelp_covid_bool_df)
    
 #TODO: data preprocessing
 
-
+'''
 st.write("Then let us look at the cov19 dataset.") 
 #TODO: add some analysis not important
 
@@ -444,34 +468,31 @@ base = alt.Chart(source).mark_circle().encode(
     y=inf_rate_target + ':Q',
 )
 st.write(base)
+'''
 
-st.write("## 2. Category and business state")
+st.write("## 3. How businesses' categories affect their reaction?")
 
-st.markdown("Let's now explore how businesses of different categories behave. We start by looking at whether different categories behave differently on having the above COVID features.")
+st.markdown("Let's now explore how businesses of different categories behave. We start by looking at whether different categories react differently with the above COVID features. Recall 1 represents true and 0 represents false. ")
 business_category_info = get_category(yelp_join)
 show_business_in_category(yelp_covid_bool_df, business_category_info)
-# TODO: change to chart like 
 
-st.markdown("### 2.1 How long do they plan to close")
+st.markdown("### 3.1 How long do they plan to close")
 close_for_how_long(yelp_join)
 
-st.markdown("### 2.2 What do Covid Banner say")
+st.markdown("### 3.2 What do Covid Banner say")
 what_covid_banner_say(yelp_join, business_category_info)
 
-st.markdown("### 2.3 What are in the highlights")
+st.markdown("### 3.3 What are in the highlights")
 business_highlight_info = get_highlight_info(yelp_join)
 what_are_highlights(business_highlight_info)
 
-st.write("## 3. Quality and business state")
+st.write("## 4. How businesses' quality affect their reaction?")
 
 st.markdown("Does business quality before COVID-19 have some relationship with their state during COVID-19? We would look at their popularity, measured by review counts, and their ratings.")
 
-st.markdown("### 3.1 Quality overview")
+st.markdown("### 4.1 Quality overview")
 show_quality_summary(yelp_join)
 
-st.markdown("### 3.2 Stars, review counts, and COVID features")
+st.markdown("### 4.2 Stars, review counts, and COVID features")
 quality_vs_covid_feature(yelp_covid_bool_df, yelp_join)
 
-# TODO: state/city change
-
-st.write("Hmm 🤔, is there some correlation between body mass and flipper length? Let's make a scatterplot with [Altair](https://altair-viz.github.io/) to find.")
